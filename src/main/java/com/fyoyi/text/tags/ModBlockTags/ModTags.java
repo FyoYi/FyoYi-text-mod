4785756f7ac12ba5831bda92e4ee9675b54d3803
FyoYi <3281079218@qq.com>
2025-09-21 20:21:04 +0800

    2025/9/21 第一次提交，加入测试物品（Text Item）,红宝石，红宝石剑，镶红宝石剑类等物品，经过测试，有未知报错，但可以正常启动游戏

--- src/main/java/com/fyoyi/text/tags/ModBlockTags/ModTags.java
+++ src/main/java/com/fyoyi/text/tags/ModBlockTags/ModTags.java
@@ -9,9 +9,4 @@ import net.minecraft.world.level.block.Block;
 
 public class ModTags {
 
-    public static class Blocks{
-        public static final TagKey<Block> MULTI_TOOL_EFFICIENT =
-                BlockTags.create(ResourceLocation.parse(text_mod.MOD_ID + ":multi_tool_efficient"));
-    }
-
 }
